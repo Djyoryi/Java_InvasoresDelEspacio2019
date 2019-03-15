87309b85e06c93b0d0e8dfdd6cfab8e9c9ebea27
xp <xp@CETYSDAM017>
2019-03-15 12:32:18 +0100

    POR SIAKA

--- src/codigo/Nave.java
+++ src/codigo/Nave.java
@@ -19,6 +19,8 @@ public class Nave {
     public Image imagen = null;
     public int x = 0;
     public int y = 0;
+    private boolean pulsadoIzquierda = false;
+    private boolean pulsadoDerecha = false;
     
     //constructor que inicializa el objeto que se llama igual que la clase y no devuleve nada (no es void ni boolean....)
     public Nave(){
@@ -28,4 +30,33 @@ public class Nave {
             //quitamos el logger porque no lo vamos a usar
         }
     }
+    
+    public void mueve(){
+        if (pulsadoIzquierda){
+        x--;
+        } 
+        if(pulsadoDerecha){
+        x++;
+        }
+    
+    }
+
+    public boolean isPulsadoIzquierda() {
+        return pulsadoIzquierda;
+    }
+
+    public void setPulsadoIzquierda(boolean pulsadoIzquierda) {
+        this.pulsadoIzquierda = pulsadoIzquierda;
+        this.pulsadoDerecha = false;
+    }
+
+    public boolean isPulsadoDerecha() {
+        return pulsadoDerecha;
+    }
+
+    public void setPulsadoDerecha(boolean pulsadoDerecha) {
+        this.pulsadoDerecha = pulsadoDerecha;
+        this.pulsadoIzquierda = false;
+    }
+    
 }
